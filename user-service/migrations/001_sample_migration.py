e96cc7c141f07387d595b0161da40ea4f5bb698c
Ezekiel Samatua <zekesamatua@gmail.com>
2023-02-16 00:29:53 +0000

    Updated YAML file to have containers running properly

--- user-service/migrations/001_sample_migration.py
+++ user-service/migrations/001_sample_migration.py
@@ -2,37 +2,21 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE dummy (
-            id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
-        );
+        CREATE TABLE users (
+            user_id serial not null primary key,
+            first_name varchar(100) not null,
+            last_name varchar(100) not null,
+            phone_number int not null,
+            email varchar(100) not null,
+            address varchar(200) not null,
+            car varchar(100) default null,
+            license_number varchar(100) default null,
+            is_swooper bool default false
+            );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE dummy;
+        DROP TABLE users;
         """
     ],
-    [
-        # "Up" SQL statement
-        """
-        CREATE TABLE big_dummy (
-            id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
-        );
-        """,
-        # "Down" SQL statement
-        """
-        DROP TABLE big_dummy;
-        """
-    ]
 ]
